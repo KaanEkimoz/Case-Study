5da620043df72bdaa8b2b0b71e2150de6512628e
Yusuf Kaan Usta <205040053@stu.istinye.edu.tr>
2025-06-20 11:40:24 +0300

    feat: add camera shake on sword hit
    Implemented a camera shake effect triggered on successful sword hits to enhance impact feedback during combat.

--- Assets/_Scripts/Combat/WeaponHitbox.cs
+++ Assets/_Scripts/Combat/WeaponHitbox.cs
@@ -1,17 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Tiny;
 
 public class WeaponHitbox : MonoBehaviour
 {
     [SerializeField] private float weaponBaseDamage = 10f;
     [SerializeField] private LayerMask targetMask;
     [SerializeField] private BoxCollider boxCollider;
+    [SerializeField] private Trail trail;
 
     private float _additionalDamage;
     private List<Collider> _alreadyHit = new();
-
+    
     public UnityEvent OnHit;
+    public UnityEvent OnActivated;
+    public UnityEvent OnDeactivated;
     private void OnTriggerEnter(Collider other)
     {
         if (_alreadyHit.Contains(other)) return;
@@ -25,19 +29,19 @@ public class WeaponHitbox : MonoBehaviour
         }
         _alreadyHit.Add(other);
     }
-    public void ActivateWeaponHitbox(float extraDamageFromOtherSources)
+    public void ActivateWeaponHitbox(float extraDamageFromOtherSources, bool isHeavyAttack = false)
     {
         _alreadyHit.Clear();
         _additionalDamage = extraDamageFromOtherSources;
         boxCollider.enabled = true;
-        
+        OnActivated?.Invoke();
     }
     public void DisableWeaponHitbox()
     {
         _alreadyHit.Clear();
         _additionalDamage = 0;
         boxCollider.enabled = false;
-        
+        OnDeactivated?.Invoke();
     }
 }
 
